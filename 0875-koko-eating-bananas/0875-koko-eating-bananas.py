43527b97e15850c4311aea2cc16d6b5ba59645cc
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-04-27 20:37:01 -0400

    Time: 199 ms (18.69%), Space: 20.7 MB (44.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,21 +1,27 @@
 from math import ceil
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        left = 1
-        right = max(piles)
-        best = right
-        while left <= right:
-            mid = (left + right) // 2
-            current = 0
+        l = 1
+        r = max(piles)
+        best = float("inf")
+
+        #[3,6,7,11].  1,2,3,[4,5],6,7,8,9,10,11
+        while l <= r:
+            mid = (l + r) // 2
+            hours = 0
             for i in range(len(piles)):
-                current += math.ceil(piles[i] / mid)
-             
-            if current <= h:
-                right = mid - 1
-                best = min(best, mid)
-            else:
-                left = mid + 1
+                hours += math.ceil(piles[i] / mid)
             
+            if hours > h:
+                l = mid + 1
+            elif hours <= h:
+                best = min(best, mid)
+                r = mid - 1
+            
+        
         return best
+
+        
+       
 
         
