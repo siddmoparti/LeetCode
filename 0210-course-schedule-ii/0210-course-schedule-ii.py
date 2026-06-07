eacd0bb7303f1cc47efdc513a073459898a50cbf
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-06-07 14:12:48 -0400

    Time: 30 ms (5.12%), Space: 20.9 MB (35.56%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,30 +1,36 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = {i: [] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            adj[crs].append(pre)
-        
         res = []
-        visit, cycle = set(), set()
+        adj = [[] for i in range(numCourses)]
+        for crs,pre in prerequisites:
+            adj[crs].append(pre)
+        visited = set()
 
         def dfs(crs):
-            if crs in cycle:
-                return False
-            if crs in visit:
-                return True
-            
-            cycle.add(crs)
-            for pre in adj[crs]:
-                if not dfs(pre):
-                    return False
-            
-            cycle.remove(crs)
-            visit.add(crs)
-            res.append(crs)
-            return True
+            if crs in visited:
+                return []
+            if crs in res:
+                return
+            if not adj[crs]:
+                res.append(crs)
+                return
 
-        for c in range(numCourses):
-            if not dfs(c):
+            visited.add(crs)
+
+            for pre in adj[crs]:
+                if dfs(pre) == []:
+                    return []
+            
+            visited.remove(crs)
+            adj[crs] = []
+            res.append(crs)
+            return
+        
+
+        for i in range(numCourses):
+            if dfs(i) == []:
                 return []
         
         return res
+
+        
