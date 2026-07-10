408594884549b7d5ec59fdf6bd7618c92fa93010
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-07-10 11:47:50 -0400

    Time: 288 ms (9.07%), Space: 26.8 MB (21.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,30 +3,40 @@ class Solution:
         ROWS = len(grid)
         COLS = len(grid[0])
         islands = 0
-
         visited = set()
-        q = collections.deque()
-        
-        def bfs(r,c):
+
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r, c))
+            visited.add((r, c))
+
             while q:
-                r,c = q.pop()
-                dir = [[0,1], [1,0], [-1,0], [0,-1]]
-                for dr,dc in dir:
-                    nr = dr + r
-                    nc = dc + c
-                    if nr in range(ROWS) and nc in range(COLS) and grid[nr][nc] == "1" and (nr,nc) not in visited:
-                        q.appendleft((nr,nc))
-                        visited.add((nr,nc))         
-            return
-        
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    q.appendleft((i,j))
-                    visited.add((i,j))
-                    bfs(i,j)
+                r, c = q.popleft()
+
+                directions = [
+                    [0, 1],
+                    [1, 0],
+                    [-1, 0],
+                    [0, -1]
+                ]
+
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
+
+                    if (
+                        nr in range(ROWS)
+                        and nc in range(COLS)
+                        and grid[nr][nc] == "1"
+                        and (nr, nc) not in visited
+                    ):
+                        q.append((nr, nc))
+                        visited.add((nr, nc))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
                     islands += 1
 
         return islands
-        
-        
