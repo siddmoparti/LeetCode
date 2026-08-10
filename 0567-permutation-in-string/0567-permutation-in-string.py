e03ee1d12a4004c9e11e3ac64a0c2d5f2bf9344d
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-08-10 13:08:48 -0400

    Time: 17 ms (64.33%), Space: 19.4 MB (53.5%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,35 +1,31 @@
-from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-
-        s1Count = {}
-        s2Count = {}
-
-        for c in s1:
-            s1Count[c] = s1Count.get(c, 0) + 1
-
-        # first window
+        counts = Counter(s1)
+        current = {}
         for i in range(len(s1)):
-            s2Count[s2[i]] = s2Count.get(s2[i], 0) + 1
-
-        if s1Count == s2Count:
+            current[s2[i]] = current.get(s2[i], 0) + 1
+        
+        if current == counts:
             return True
+        i = 0
+        n = len(s1) - 1
+        m = len(s2) - 1
+        
 
-        left = 0
+        while n < m:
+            current[s2[i]] -= 1
+            if current[s2[i]] == 0:
+                del current[s2[i]]
 
-        for right in range(len(s1), len(s2)):
-            s2Count[s2[right]] = s2Count.get(s2[right], 0) + 1
-            removeChar = s2[left]
-            s2Count[removeChar] -= 1
-
-            if s2Count[removeChar] == 0:
-                del s2Count[removeChar]
-
-            left += 1
-
-            if s1Count == s2Count:
+            i += 1
+            n += 1
+            current[s2[n]] = current.get(s2[n], 0) + 1
+            if current == counts:
                 return True
+        
         return False
+       
 
+      
