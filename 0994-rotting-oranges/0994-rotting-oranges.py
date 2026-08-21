3ff0c12bc8f1e312714c1934ed8a5deeae2b75a9
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-08-20 20:42:37 -0400

    Time: 7 ms (15.72%), Space: 19.3 MB (79.02%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,36 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        minutes = 0
         fresh = 0
+        minutes = 0
         q = collections.deque()
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 1:
                     fresh += 1
                 elif grid[i][j] == 2:
-                    q.appendleft((i,j))
+                    q.append((i,j))
 
         while q and fresh > 0:
-            q_length = len(q)
-
-            for i in range(q_length):
-                r,c = q.pop()
-                dir = [[1,0],[0,1],[0,-1], [-1,0]]
+            rotten = len(q)
+            for i in range(rotten):
+                dir = [[0,1], [1,0], [0,-1], [-1,0]]
+                r,c = q.popleft()
                 for dr,dc in dir:
                     nr = dr + r
                     nc = dc + c
-                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == 1:
+                    if nr in range(len(grid)) and nc in range(len(grid[0])) and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
-                        q.appendleft((nr,nc))
                         fresh -= 1
-            
+                        q.append((nr,nc))
             minutes += 1
-                
         
         if fresh == 0:
             return minutes
-        elif fresh != 0:
+        else:
             return -1
         
