55135829a7af30fd85b8ebf34ca814ca6fd57a31
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-08-22 13:19:38 -0400

    Time: 1 ms (41.41%), Space: 19.5 MB (34.92%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,20 +1,31 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = []
-        res = []
-
-        def backtrack(open, close):
-            if open == n and  close == n:
-                res.append("".join(stack))
-                return
-            if open < n:
-                stack.append("(")
-                backtrack(open + 1, close)
-                stack.pop()
-            if close < open:
-                stack.append(")")
-                backtrack(open, close + 1)
-                stack.pop()
         
-        backtrack(0,0)
+
+        open = 0
+        closed = 0
+        res = []
+        self.options = ['(', ')']
+        def dfs(open, closed, cur):
+            if open > n:
+                return
+            if closed > n:
+                return
+            if open == n and closed == n:
+                res.append("".join(cur))
+                return
+            if closed > open:
+                return
+            
+            for c in self.options:
+                cur.append(c)
+                if c == '(':
+                    dfs(open + 1, closed, cur)
+                else:
+                    dfs(open, closed + 1, cur)
+                cur.pop()
+
+        dfs(0,0, [])
         return res
+        
+        
