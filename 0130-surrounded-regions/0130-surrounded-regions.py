68c7b38cc1793f9409b4a14de28a4c65a6c7d856
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-08-24 12:11:57 -0400

    Time: 65 ms (5.04%), Space: 31.4 MB (7.05%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,39 +1,44 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         rows = len(board)
         cols = len(board[0])
+
+        q = collections.deque()
         visited = set()
 
-        def bfs(r, c):
-            q = deque()
-            q.append((r, c))
-            visited.add((r, c))
-
+        def bfs(r,c):
+            q = collections.deque([(r,c)])
+            visited.add((r,c))
+            
+        
+            cur_path = set()
+            edge = False
             while q:
-                r, c = q.popleft()
-
-                directions = [[1,0], [0,1], [-1,0], [0,-1]]
-                for dr, dc in directions:
-                    nr = r + dr
-                    nc = c + dc
-
-                    if nr in range(rows) and nc in range(cols) and (nr, nc) not in visited and board[nr][nc] == "O":
-                        visited.add((nr, nc))
-                        q.append((nr, nc))
-
-        for r in range(rows):
-            if board[r][0] == "O" and (r, 0) not in visited:
-                bfs(r, 0)
-            if board[r][cols - 1] == "O" and (r, cols - 1) not in visited:
-                bfs(r, cols - 1)
-
-        for c in range(cols):
-            if board[0][c] == "O" and (0, c) not in visited:
-                bfs(0, c)
-            if board[rows - 1][c] == "O" and (rows - 1, c) not in visited:
-                bfs(rows - 1, c)
-
+                r,c = q.popleft()
+                cur_path.add((r,c))
+                if r == 0 or r == rows - 1 or c == 0 or c == cols - 1:
+                    edge = True
+                dir = [[-1,0], [1,0], [0,-1], [0,1]]
+                for dr,dc in dir:
+                    nr = dr + r
+                    nc = dc + c
+                    if nr in range(rows) and nc in range(cols) and (nr,nc) not in visited and board[nr][nc] == 'O':
+                        q.append((nr,nc))
+                        visited.add((nr,nc))
+                    
+            if not edge:
+                for r,c in cur_path:
+                    board[r][c] = 'X'         
+            return
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "O" and (r, c) not in visited:
-                    board[r][c] = "X"
+                if (r,c) not in visited and board[r][c] == 'O':
+                    bfs(r,c)
+                    
+
+            
+
+        
