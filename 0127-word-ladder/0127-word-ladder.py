0a4a7182bf6ee9f39bb58ac0f89e32dc078a80d5
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-08-31 11:27:35 -0400

    Time: 6181 ms (6.2%), Space: 19.9 MB (99.21%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,36 +1,33 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
-        visited = set()
-        visited.add(beginWord)
         q = collections.deque()
         q.append(beginWord)
         n = len(endWord)
-
-        res = 1
+        res = 0
+        visited = set()
+        visited.add(beginWord)
         while q:
-            for _ in range(len(q)):
-
-                cur = q.popleft()
-                if cur == endWord:
+            res += 1
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
                     return res
-                found_next = False
-                visited.add(cur)
-                for word in wordList:
-                    if word in visited:
-                        continue
-                
+                for w in wordList:
                     diff = 0
                     for i in range(n):
-                        if word[i] != cur[i]:
+                        if w[i] != word[i]:
                             diff += 1
                         if diff > 1:
                             break
-                    if diff == 1:
-                        visited.add(word)
-                        q.append(word)
-            res += 1
-                        
+                    
+                    if diff == 1 and w not in visited:
+                        visited.add(w)
+                        q.append(w)
 
         return 0
+        # hit->hot->dot,lot->dog,log->cog
+        
+        
+        
+                
+        
