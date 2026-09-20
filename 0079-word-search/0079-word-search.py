79d88dd496c00fd6b88af20d71118053c9b10a58
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-09-20 16:14:53 -0400

    Time: 6388 ms (5.3%), Space: 19.5 MB (59.89%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,33 +1,36 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    def exist(self, board: list[list[str]], word: str) -> bool:
+        dir = [[0,1], [1,0], [0,-1], [-1,0]]
         rows = len(board)
         cols = len(board[0])
-
         visited = set()
-        def dfs(r,c, i):
-            if i == len(word):
-                return True
-            if r not in range(rows) or c not in range(cols) or (r,c) in visited:
+        def dfs(index, r, c):
+            if board[r][c] != word[index]:
                 return False
-            if board[r][c] == word[i]:
-                visited.add((r,c))
-                res = (dfs(r, c + 1, i + 1)
-                or dfs(r + 1, c, i + 1)
-                or dfs(r - 1, c, i + 1)
-                or dfs(r, c - 1, i + 1))
-                
-                visited.remove((r,c))
-                return res
-            
-            return False
-        
-        for i in range(rows):
-            for j in range(cols):
-                if dfs(i,j, 0):
-                    return True
-                
 
+            if index == len(word) - 1:
+                return True
+            visited.add((r,c))
+            
+            
+            for dr, dc in dir:
+                nr = r + dr
+                nc = c + dc
+                if nr in range(rows) and nc in range(cols) and (nr,nc) not in visited:
+                    if dfs(index + 1, nr, nc):
+                        return True
+        
+            visited.remove((r,c))
+            return False
+            
+        
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(0, r,c):
+                    return True
+        
         return False
 
 
-        
+                
+            
