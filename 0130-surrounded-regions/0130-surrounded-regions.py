5eb9094a06d4f6197c5cd7adbdf9b4636fc64caf
siddmoparti <158665540+siddmoparti@users.noreply.github.com>
2026-09-22 13:06:09 -0400

    Time: 8 ms (29.27%), Space: 22.5 MB (30.87%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,44 +1,53 @@
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+    def solve(self, board: list[list[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows = len(board)
-        cols = len(board[0])
-
-        q = collections.deque()
+        m = len(board)
+        n = len(board[0])
         visited = set()
-
+        safe = set()
         def bfs(r,c):
-            q = collections.deque([(r,c)])
+            if (r,c) in visited:
+                return
             visited.add((r,c))
-            
-        
-            cur_path = set()
-            edge = False
+
+            q = collections.deque()
+            q.append((r,c))
             while q:
                 r,c = q.popleft()
-                cur_path.add((r,c))
-                if r == 0 or r == rows - 1 or c == 0 or c == cols - 1:
-                    edge = True
-                dir = [[-1,0], [1,0], [0,-1], [0,1]]
+                dir = [[0,1], [1,0], [-1,0], [0,-1]]
                 for dr,dc in dir:
                     nr = dr + r
                     nc = dc + c
-                    if nr in range(rows) and nc in range(cols) and (nr,nc) not in visited and board[nr][nc] == 'O':
+                    if nr in range(m) and nc in range(n) and (nr,nc) not in visited and board[nr][nc] == 'O':
                         q.append((nr,nc))
+                        safe.add((nr,nc))
                         visited.add((nr,nc))
-                    
-            if not edge:
-                for r,c in cur_path:
-                    board[r][c] = 'X'         
-            return
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) not in visited and board[r][c] == 'O':
-                    bfs(r,c)
-                    
 
-            
+        for r in range(len(board)):
+            if board[r][0] == 'O' and (r,0) not in visited:
+                safe.add((r,0))
+                bfs(r, 0)
+        for c in range(len(board[0])):
+            if board[0][c] == 'O' and (0,c) not in visited:  
+                safe.add((0,c))
+                bfs(0, c)
+        for c in range(len(board[0])):
+            if board[m - 1][c] == 'O' and (m-1,c) not in visited:
+                safe.add((m-1,c))
+                bfs(m - 1, c)
+        
+        for r in range(len(board)):
+            if board[r][ n - 1] == 'O' and (r, n -1) not in visited:
+                safe.add((r,n-1))
+                bfs(r, n - 1)
+        
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == 'O' and (r,c) not in safe:
+                    board[r][c] = 'X'
+        
 
+        
         
